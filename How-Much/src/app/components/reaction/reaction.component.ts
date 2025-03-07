dc686fe934238a1463ceab23d9c7ff04ff775985
Sebastian Moksnes <sebastian.moksnes@cab.se>
2025-03-07 14:09:59 +0100

    animation

--- How-Much/src/app/components/reaction/reaction.component.ts
+++ How-Much/src/app/components/reaction/reaction.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import {Component, Input} from '@angular/core';
+import {trigger, transition, state, animate, style, AnimationEvent} from '@angular/animations';
+
 
 @Component({
   selector: 'reaction',
@@ -7,17 +8,52 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   templateUrl: './reaction.component.html',
   styleUrl: './reaction.component.scss',
   animations: [
-    trigger('flowInFadeOut', [
-      state('void', style({ transform: 'translateY(100%)', opacity: 0 })),
-      state('*', style({ transform: 'translateY(0)', opacity: 1 })),
-      transition('void => *', [
-        animate('1s ease-out')
-      ]),
-      transition('* => void', [
-        animate('1s ease-in', style({ opacity: 0 }))
-      ])
-    ])
-  ]
+    trigger('openClose', [
+      // ...
+      state(
+        'open',
+        style({
+          height: '200px',
+          opacity: 1,
+          backgroundColor: 'yellow',
+        }),
+      ),
+      state(
+        'closed',
+        style({
+          height: '100px',
+          opacity: 0.8,
+          backgroundColor: 'blue',
+        }),
+      ),
+      transition('open => closed', [animate('1s')]),
+      transition('closed => open', [animate('0.5s')]),
+      transition('* => closed', [animate('1s')]),
+      transition('* => open', [animate('0.5s')]),
+      transition('open <=> closed', [animate('0.5s')]),
+      transition('* => open', [animate('1s', style({opacity: '*'}))]),
+      transition('* => *', [animate('1s')]),
+    ]),
+  ],
 })
 export class ReactionComponent {
+  isOpen = true;
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+  onAnimationEvent(event: AnimationEvent) {
+
+    // openClose is trigger name in this example
+    console.warn(`Animation Trigger: ${event.triggerName}`);
+    // phaseName is "start" or "done"
+    console.warn(`Phase: ${event.phaseName}`);
+    // in our example, totalTime is 1000 (number of milliseconds in a second)
+    console.warn(`Total time: ${event.totalTime}`);
+    // in our example, fromState is either "open" or "closed"
+    console.warn(`From: ${event.fromState}`);
+    // in our example, toState either "open" or "closed"
+    console.warn(`To: ${event.toState}`);
+    // the HTML element itself, the button in this case
+    console.warn(`Element: ${event.element}`);
+  }
 }
